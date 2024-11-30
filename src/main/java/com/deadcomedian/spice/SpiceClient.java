ff44bf661dbff58c65048481caabe3a08c5cc624
The-Dead-Comedian <113227369+The-Dead-Comedian@users.noreply.github.com>
2024-11-30 22:21:39 +0200

    Initial commit

---
+++ src/main/java/com/deadcomedian/spice/SpiceClient.java
@@ -0,0 +1,13 @@
+package com.deadcomedian.spice;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpiceClient   implements ClientModInitializer {
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
+
